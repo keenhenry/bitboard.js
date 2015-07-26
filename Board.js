5424ac266813729eae7c8a9d0d209f782c5dbd0b
Henry Huang <keenhenry1109@gmail.com>
2015-07-26 16:44:13 +0200

    using module pattern for Board module

--- Board.js
+++ Board.js
@@ -6,10 +6,41 @@
 
 var module = module || {};
 
-module.Board = function() {
-    function(width, height){
-        var buf_len = width * height / 8;
-        var buffer  = new ArrayBuffer(buf_len);
-        var board   = new UInt8Array(buffer);
+module.Board = (function() {
+    'use strict';
+
+    // private variables for BitBoard class
+    var width;
+    var height;
+    var bitboard;
+
+    var numPlayers;
+    var players;
+
+    /**
+     * constructor function
+     *
+     */
+    function BitBoard (np, w, h) {
+        width      = w;
+        height     = h;
+        bitboard   = new Uint8Array(new ArrayBuffer(width * height / 8));
+
+        numPlayers = np;
+        players    = [];
     }
-}
+
+    // class methods for BitBoard class
+    /**
+     * 
+     *
+     *
+     */
+    // BitBoard.prototype.setPosition = function () {};
+    // BitBoard.prototype.getPosition = function () {};
+    // BitBoard.prototype.getBoard    = function () {};
+
+    // return a module!
+    return {
+    };
+})();
