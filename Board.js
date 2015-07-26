dd5f8b731ddccb2bebf1bce184b552e94deed8d9
Henry Huang <keenhenry1109@gmail.com>
2015-07-26 19:04:33 +0200

    created primary implementation for Board.js

--- Board.js
+++ Board.js
@@ -1,5 +1,18 @@
 /**
- * Provides generic Board module for writing board games
+ * Provides generic Board module for writing board games.
+ * An example of an 8x8 Board is depicted as follows: 
+ *
+ *             01234567  (X)
+ *            0........
+ *            1........
+ *            2........
+ *            3........
+ *            4........
+ *            5........
+ *            6........
+ *            7........
+ *            
+ *           (Y)
  *
  * @module Board
  */ 
@@ -18,8 +31,12 @@ module.Board = (function() {
     var players;
 
     /**
-     * constructor function
+     * Represents a BitBoard: https://chessprogramming.wikispaces.com/Bitboards
      *
+     * @constructor
+     * @param {number} np - number of players for this bitboard
+     * @param {number} w - width of this bitboard
+     * @param {number} h - height this bitboard
      */
     function BitBoard (np, w, h) {
         width      = w;
@@ -30,17 +47,36 @@ module.Board = (function() {
         players    = [];
     }
 
-    // class methods for BitBoard class
-    /**
+    /** @function setPosition
      * 
-     *
-     *
+     * @param {number} player - the player for which the postion to be set
+     * @param {number} x - the x coordinate of the postion
+     * @param {number} y - the y coordinate of the postion
      */
-    // BitBoard.prototype.setPosition = function () {};
-    // BitBoard.prototype.getPosition = function () {};
+    BitBoard.prototype.setPosition = function (player, x, y) {
+        bitboard[y]      = 128 >>> x;
+        players [player] = y*height + x;
+    };
+
+    /** @function setPosition
+     * 
+     * @param {number} player - the player for which the postion to be retrieved
+     * @returns {Object} - the x and y coordinate in Object literal format
+     */
+    BitBoard.prototype.getPosition = function (player) {
+        var pos = players[player];
+
+        return {
+            x: pos % 8,
+            y: pos / 8
+        };
+    };
+
     // BitBoard.prototype.getBoard    = function () {};
 
-    // return a module!
+    // return a module/namespace!
     return {
+        BitBoard: BitBoard
+        // init: ;
     };
 })();
