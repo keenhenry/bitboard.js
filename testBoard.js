c6d4faffd6612663cadb8f212ad48abcf182f5f5
Henry Huang <keenhenry1109@gmail.com>
2015-07-28 21:35:22 +0200

    implemented bootstrapping testing code

---
+++ testBoard.js
@@ -0,0 +1,15 @@
+var requirejs = require('requirejs');
+
+requirejs.config({
+    //Pass the top-level main.js/index.js require
+    //function to requirejs so that node modules
+    //are loaded relative to the top-level JS file.
+    nodeRequire: require
+});
+
+requirejs(['Board'], function (Board) {
+    var b = Board.initBitBoard(2, 8, 8);
+
+    b.printBoard();
+    
+});
