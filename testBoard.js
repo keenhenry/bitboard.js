07c9ea2bdf4dcff5b145cc1024833ae694a11ef1
Henry Huang <keenhenry1109@gmail.com>
2015-07-29 22:11:50 +0200

    exercise module code after new improvements

--- testBoard.js
+++ testBoard.js
@@ -8,21 +8,35 @@ requirejs.config({
 });
 
 requirejs(['BitBoard'], function (mBitBoard) {
-    var b = mBitBoard.initBitBoard(2, 8, 8);
 
-    // print out board state to console
-    console.log("BOARD B STATE:");
-    b.setPosition(0, 3, 0);
-    b.printBoard();
+    console.log("BOARD INITIALIZED:");
+    var b1 = mBitBoard.initBitBoard(2, 8, 8);
+    mBitBoard.printBoard(b1.bitboard);
 
-    console.log("BOARD A STATE:");
-    var a = mBitBoard.initBitBoard(2, 8, 8);
-    a.setPosition(0, 3, 5);
-    a.setPosition(0, 4, 6);
-    a.printBoard();
+    console.log("BOARD UPDATE player 0 position to (3,0):");
+    b1.setPosition(0, 3, 0);
+    mBitBoard.printBoard(b1.bitboard);
+    console.log("BOARD UPDATE player 0 position to (4,0):");
+    b1.setPosition(0, 4, 0);
+    mBitBoard.printBoard(b1.bitboard);
+    console.log("BOARD UPDATE player 1 position to (1,1):");
+    b1.setPosition(1, 1, 1);
+    mBitBoard.printBoard(b1.bitboard);
+    console.log("BOARD UPDATE player 1 position to (1,5):");
+    b1.setPosition(1, 1, 5);
+    mBitBoard.printBoard(b1.bitboard);
 
-    console.log("BOARD B STATE:");
-    b.printBoard();
-    b.setPosition(0, 3, 0);
-    b.printBoard();
+    console.log("ANOTHER BOARD INITIALIZED:");
+    var b2 = mBitBoard.initBitBoard(2, 8, 8);
+    mBitBoard.printBoard(b2.bitboard);
+    console.log("PREVIOUS BOARD:");
+    mBitBoard.printBoard(b1.bitboard);
+    console.log("BOARD UPDATE player 1 position to (1,5):");
+    b2.setPosition(1, 1, 5);
+    mBitBoard.printBoard(b2.bitboard);
+    console.log("player 1 position should be (1,5):");
+    var pos = b2.getPosition(1);
+    console.assert(pos.x==1 && pos.y==5);
+    console.log(pos);
+
 });
